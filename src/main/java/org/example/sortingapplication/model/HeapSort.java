feb2443f13a7373c3842f6d5d22ecd1c54f70b61
Nethmini Rajapaksha <nethminirajapaksha2001@gmail.com>
2024-11-16 21:27:03 +0530

    SortMaster/Nethmini : Implement Heap Sort

---
+++ src/main/java/org/example/sortingapplication/model/HeapSort.java
@@ -0,0 +1,44 @@
+package org.example.sortingapplication.model;
+
+import java.util.List;
+
+public class HeapSort {
+    public long sort(List<Double> data) {
+        long startTime = System.nanoTime();
+
+        int n = data.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(data, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            double temp = data.get(0);
+            data.set(0, data.get(i));
+            data.set(i, temp);
+
+            heapify(data, i, 0);
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1_000_000; // Time in milliseconds
+    }
+
+    private void heapify(List<Double> data, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && data.get(left) > data.get(largest)) {
+            largest = left;
+        }
+        if (right < n && data.get(right) > data.get(largest)) {
+            largest = right;
+        }
+        if (largest != i) {
+            double swap = data.get(i);
+            data.set(i, data.get(largest));
+            data.set(largest, swap);
+
+            heapify(data, n, largest);
+        }
+    }
+}
