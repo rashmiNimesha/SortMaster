ceefbdae917418bb54218a8467aac7b0c549d4c1
s3h4n <shehanrweerasekara@gmail.com>
2024-11-20 21:36:02 +0530

    Add: Performance controller

--- src/main/java/org/example/sortingapplication/controller/SortingController.java
+++ src/main/java/org/example/sortingapplication/controller/SortingController.java
@@ -1,11 +1,28 @@
 package org.example.sortingapplication.controller;
 
 import org.example.sortingapplication.model.HeapSort;
+import org.example.sortingapplication.model.MergeSort;
+import org.example.sortingapplication.model.QuickSort;
 import org.example.sortingapplication.model.ShellSort;
 
 import java.util.List;
 
 public class SortingController {
+    public long runQuickSort(List<Double> data) {
+        long startTime = System.nanoTime();
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(data);
+        long endTime = System.nanoTime();
+        return (endTime - startTime) ;
+    }
+
+    public long runMergeSort(List<Double> data) {
+        long startTime = System.nanoTime();
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(data);
+        long endTime = System.nanoTime();
+        return (endTime - startTime);
+    }
 
     public long runShellSort(List<Double> dataItem) {
         long startTime = System.nanoTime();
@@ -20,7 +37,7 @@ public class SortingController {
         HeapSort heapSort = new HeapSort();
         heapSort.sort(data);
         long endTime = System.nanoTime();
-        return (endTime - startTime) ; // Convert to milliseconds
+        return (endTime - startTime) ;
     }
 
 }
