df691e2cc60100cf8d4b30bab44effb501d64a32
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2024-11-24 10:57:03 +0530

    SortMaster/rashmi-implement About scene.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.sortingapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens org.example.sortingapplication.controller to javafx.fxml;
     exports org.example.sortingapplication;
